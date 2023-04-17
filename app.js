904272daea9be296897db0b5a6fb818f573ee004
par-booze <pravesh.boozedhur@publicis.mu>
2023-04-17 17:16:46 +0400

    TestCommit

---
+++ app.js
@@ -0,0 +1,24 @@
+// ****** SELECT ITEMS **********
+
+
+const submitbtn = document.querySelector(".submit-btn");
+
+const listarr = [];
+
+
+// ****** EVENT LISTENERS **********
+submitbtn.addEventListener("click",function(e){
+    e.preventDefault();
+    const input = document.getElementById("grocery").value;
+    listarr.push[input[i]];
+    console.log(input);
+ console.log("clicked")
+})
+
+console.log(listarr);
+
+// ****** FUNCTIONS **********
+
+// ****** LOCAL STORAGE **********
+
+// ****** SETUP ITEMS **********
